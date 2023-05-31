d5dcef0e64693397018f8bf1b645dfd0897a12cd
bdotgeorge <bzzz2009@yandex.ru>
2023-05-31 22:05:11 +0400

    my method first part

--- finalWork.py
+++ finalWork.py
@@ -1,10 +1,42 @@
 import random
 import pandas as pd
 
-lst = ['robot'] * 10
-lst += ['human'] * 10
-random.shuffle(lst)
-data = pd.DataFrame(lst)
-category = set(pd[])
-print(category)
-#print(pd.get_dummies(data))
+
+def pandasMethod():
+    lst = ['robot'] * 10
+    lst += ['human'] * 10
+    random.shuffle(lst)
+    data = pd.DataFrame({'whoAmI': lst})
+    #pd.get_dummies(data)
+    data['temp'] = 1 #added temp value == 1
+    data.set_index([data.index, 'whoAmI'], inplace=True)
+    data = data.unstack(level=-1, fill_value=0).astype(int)
+    data.columns = data.columns.droplevel() #
+    data.columns.name = None #remove name who am i
+    print(f'Pandas method\n{data}')
+
+def myMethod():
+    print('My method\n')
+    lst = ['robot'] * 10
+    lst += ['human'] * 10
+    random.shuffle(lst)
+    data = pd.DataFrame({'whoAmI': lst})    
+    #print(pd.get_dummies(data))
+    columns = dict()
+    value = list()
+    """    for i, j in enumerate(data):
+        print(f'data {i, j}\n')
+    """
+    for i in data:#to all columns
+        for k, j in enumerate(data[i].unique().tolist()):
+            columns[j] = k
+    
+    for i in data:
+        for d in data[i]:
+            columns.get(d)
+    print(data.size)
+    print(columns)
+
+
+#pandasMethod()
+myMethod()
