e2fbc7a67428efc153b36f5738734ab60173ac09
bdotgeorge <bzzz2009@yandex.ru>
2023-06-02 16:33:49 +0400

    work done

--- finalWork.py
+++ finalWork.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 
 def firstMethod():
+    """
+    with panda functions
+    """
     lst = ['robot'] * 10
     lst += ['human'] * 10
     random.shuffle(lst)
@@ -13,38 +16,47 @@ def firstMethod():
     data = data.unstack(level=-1, fill_value=0).astype(int)
     data.columns = data.columns.droplevel() #
     data.columns.name = None #remove name who am i
-    print(f'Pandas method\n{data}')
+    print(data.head(20))
 
 def secondMethod():
-    print('My method\n')
+    """
+    without pandas functions, 
+    plus write and read file        
+    """
     lst = ['robot'] * 10
     lst += ['human'] * 10
+    #lst += ['animal'] * 10
     random.shuffle(lst)
     data = pd.DataFrame({'whoAmI': lst})    
-    #print(pd.get_dummies(data))
-    columns = dict()
-    value = list()
-    """    for i, j in enumerate(data):
-        print(f'data {i, j}\n')
-    """
+    print(pd.get_dummies(data))
+    columns = list()
     for i in data:#to all columns
-        for k, j in enumerate(data[i].unique().tolist()):
-            columns[j] = k
-    countCol = 0#len(columns)
-    file = open('oneHot.csv', 'w', encoding='utf-8')
-    for c in columns:
-        file.write(str(c) + ', ')
-    file.write('\n')
+        for j in data[i].unique().tolist():
+            columns.append('whoAmI_' + str(j))
+    value = ''
+    for c in range (0, len(columns)):
+        value += str(columns[c])
+        if c < len(columns) - 1: 
+            value += ','
     for i in data:
         for d in data[i]:
-            countCol +=1
-            file.write(str(columns.get(d)) + ', ')
-            if countCol == len(columns):
-                countCol = 0
-                file.write('\n')
+            value += '\n'
+            for v in range (0, len(columns)):
+                if d in columns[v]:
+                    value += str(1)
+                else:
+                    value += str(0)
+                if v < len(columns) - 1: value += ','
+    
+    file = open('oneHot.csv', 'w', encoding='utf-8')
+    file.write(value)
     file.close()
+    
+    d = pd.read_csv('oneHot.csv')
+    print(f'from csv file\n {d.head(20)}') 
+    print(f'from get_dummies file\n{pd.get_dummies(d)}')
 
 
 
-#firstMethod()
+firstMethod()
 secondMethod()
